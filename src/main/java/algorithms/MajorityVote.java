7360e094ebd767e63dcf8530c008a9e4e39ffa00
Aiymzhan <aikogirl483@gmail.com>
2025-10-04 21:18:02 +0500

    feat(algorithm): baseline majority vote  implementation

--- src/main/java/algorithms/MajorityVote.java
+++ src/main/java/algorithms/MajorityVote.java
@@ -1,4 +1,29 @@
 package algorithms;
 
 public class MajorityVote {
+    public int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+        for (int element : nums) {
+            if (count == 0) {
+                candidate = element;
+            }
+            if (element == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        //checking if majority candidate occurs more than n/2 times  by formula
+        int countCheck = 0;
+        for (int element : nums) {
+            if (element == candidate) {
+                countCheck++;
+            }
+        }
+        if (countCheck > nums.length/ 2) {
+            return candidate;
+        }
+        return -1;
+    }
 }
